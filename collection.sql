76b4a52c3e5b0f729f3e8b6b205ae05ddd1bb035
Fra Diavolo <fra.diavolo@gmail.com>
2022-03-07 20:36:56 +0100

    autosave

---
+++ collection.sql
@@ -0,0 +1,59 @@
+
+
+DROP DATABASE taged_collection;
+
+CREATE DATABASE taged_collection;
+
+\c taged_collection;
+
+CREATE TABLE combat
+(
+   Id_Combat SERIAL,
+   Résultat VARCHAR(50),
+   Tier INT,
+   Rules VARCHAR(250),
+   Classe VARCHAR(50),
+   PRIMARY KEY(Id_Combat)
+);
+
+CREATE TABLE pokemon(
+   Nom VARCHAR(50),
+   PRIMARY KEY(Nom)
+);
+
+CREATE TABLE utilisateur(
+   Nom VARCHAR(50),
+   Avatar VARCHAR(50),
+   PRIMARY KEY(Nom)
+);
+
+CREATE TABLE equipe(
+   Id_Equipe SERIAL,
+   Nombre INT,
+   Pokemon1 VARCHAR(50),
+   Pokemon2 VARCHAR(50),
+   Pokemon3 VARCHAR(50),
+   Pokemon4 VARCHAR(50),
+   Pokemon5 VARCHAR(50),
+   Pokemon6 VARCHAR(50),
+   PRIMARY KEY(Id_Equipe),
+   FOREIGN KEY(Pokemon1) REFERENCES pokemon(Nom),
+   FOREIGN KEY(Pokemon2) REFERENCES pokemon(Nom),
+   FOREIGN KEY(Pokemon3) REFERENCES pokemon(Nom),
+   FOREIGN KEY(Pokemon4) REFERENCES pokemon(Nom),
+   FOREIGN KEY(Pokemon5) REFERENCES pokemon(Nom),
+   FOREIGN KEY(Pokemon6) REFERENCES pokemon(Nom)
+);
+
+CREATE TABLE engage
+(
+   Id_Combat INT,
+   Nom VARCHAR(50),
+   Id_Equipe INT,
+   ELO INT,
+   PRIMARY KEY(Id_Combat, Nom, Id_Equipe),
+   FOREIGN KEY(Id_Combat) REFERENCES combat(Id_Combat),
+   FOREIGN KEY(Nom) REFERENCES utilisateur(Nom),
+   FOREIGN KEY(Id_Equipe) REFERENCES equipe(Id_Equipe)
+);
+
--- taged/application/script/getStats.sh
+++ taged/application/script/getStats.sh
@@ -3,6 +3,22 @@
 Folder=$1
 Separator=" : "
 
+function handleOptionnal ()
+{
+    Optionnal=$1
+    OptFolder=$Folder/$Optionnal
+
+    if [ -d $OptFolder ]
+    then
+
+         echo "Nb Files $Optionnal$Separator" `find $OptFolder -type f | wc -l`
+
+    fi
+}
+
+
 echo "Nb Files$Separator" `find $Folder -type f | wc -l`
 
 echo "Size on Disk$Separator" `du -sh $Folder | awk -F ' ' ' { print $ 1 } '`
+
+handleOptionnal gen1ou
