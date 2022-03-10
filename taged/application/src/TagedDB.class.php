63b7da8bf3d4fa6ff08d51118010217f4e913c73
Fra Diavolo <fra.diavolo@gmail.com>
2022-03-10 03:14:03 +0100

    autosave

---
+++ taged/application/src/TagedDB.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class TagedDB
+{
+    protected static $DBserver = "localhost";
+    protected static $DBuser = "postgres";
+    protected static $DBpwd = "plopplopP2";
+    protected static $PDO = NULL;
+    protected static $PDOStatement = NULL;
+}
+
