9eddde427f99343c8662bffd58ae60b7171c9770
FraDiavolo7 <fra.diavolo@gmail.com>
2023-07-03 18:11:28 -0500

    Ajout CoSky validé

---
+++ taged/application/src/pages/PageTestCoSky.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class PageTestCoSky extends TagedPage
+{
+	public function __construct ( $InputData = NULL )
+	{
+		parent::__construct ( $InputData );
+		$Data = ( NULL == $InputData ? $_REQUEST : $InputData );
+		$this->PageTitle = 'TestCoSky';
+	
+		$Switch = new Switcher ( $InputData );
+		
+		$this->handle ( $Data );
+	}
+	
+	protected function handle ( $Data )
+	{
+	    $CoSky = new CoSky ( NULL );
+	    
+	    $CoSky->run ();
+	    
+	    $Data = $CoSky->getData ();
+	    
+	    $ToShow = array ();
+	    
+	    foreach ( $Data as $Attr => $Entries )
+	    {
+	        foreach ( $Entries as $Tuple => $Value )
+	        {
+	            $ToShow [$Tuple] [$Attr] = $Value;
+	        }
+	    }
+	    
+	    foreach ( $ToShow as $Tuple => $Row )
+	    {
+	        $ToShow [$Tuple] ['Score'] = $CoSky->getScore ($Tuple);
+	    }
+
+	    $this->add ( HTML::tableFull ( $ToShow, array ( 'class' => 'cuboide'), TRUE ) );
+	}
+	
+} // PageTestCoSky
